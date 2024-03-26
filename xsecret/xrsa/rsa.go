4a7923841f3dfc6a52b9c2c8d337f0fc33b83087
yanwei <yanwei@deemob.com>
2024-03-26 17:46:20 +0800

    weiyanwei

--- xsecret/xrsa/rsa.go
+++ xsecret/xrsa/rsa.go
@@ -1,81 +1,109 @@
 package xrsa
 
 import (
+	"bytes"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	"encoding/base64"
 	"encoding/pem"
 	"errors"
+	"fmt"
+	"os"
 )
 
-// 私钥生成
-//openssl genrsa -out rsa_private_key.pem 1024
-var privateKey = []byte(`
------BEGIN RSA PRIVATE KEY-----
-MIICWwIBAAKBgQDcGsUIIAINHfRTdMmgGwLrjzfMNSrtgIf4EGsNaYwmC1GjF/bM
-h0Mcm10oLhNrKNYCTTQVGGIxuc5heKd1gOzb7bdTnCDPPZ7oV7p1B9Pud+6zPaco
-qDz2M24vHFWYY2FbIIJh8fHhKcfXNXOLovdVBE7Zy682X1+R1lRK8D+vmQIDAQAB
-AoGAeWAZvz1HZExca5k/hpbeqV+0+VtobMgwMs96+U53BpO/VRzl8Cu3CpNyb7HY
-64L9YQ+J5QgpPhqkgIO0dMu/0RIXsmhvr2gcxmKObcqT3JQ6S4rjHTln49I2sYTz
-7JEH4TcplKjSjHyq5MhHfA+CV2/AB2BO6G8limu7SheXuvECQQDwOpZrZDeTOOBk
-z1vercawd+J9ll/FZYttnrWYTI1sSF1sNfZ7dUXPyYPQFZ0LQ1bhZGmWBZ6a6wd9
-R+PKlmJvAkEA6o32c/WEXxW2zeh18sOO4wqUiBYq3L3hFObhcsUAY8jfykQefW8q
-yPuuL02jLIajFWd0itjvIrzWnVmoUuXydwJAXGLrvllIVkIlah+lATprkypH3Gyc
-YFnxCTNkOzIVoXMjGp6WMFylgIfLPZdSUiaPnxby1FNM7987fh7Lp/m12QJAK9iL
-2JNtwkSR3p305oOuAz0oFORn8MnB+KFMRaMT9pNHWk0vke0lB1sc7ZTKyvkEJW0o
-eQgic9DvIYzwDUcU8wJAIkKROzuzLi9AvLnLUrSdI6998lmeYO9x7pwZPukz3era
-zncjRK3pbVkv0KrKfczuJiRlZ7dUzVO0b6QJr8TRAA==
------END RSA PRIVATE KEY-----
-`)
+type XRsa struct {
+	publicKey  *rsa.PublicKey
+	privateKey *rsa.PrivateKey
+}
 
-// 公钥: 根据私钥生成
-//openssl rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem
-var publicKey = []byte(`
------BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDcGsUIIAINHfRTdMmgGwLrjzfM
-NSrtgIf4EGsNaYwmC1GjF/bMh0Mcm10oLhNrKNYCTTQVGGIxuc5heKd1gOzb7bdT
-nCDPPZ7oV7p1B9Pud+6zPacoqDz2M24vHFWYY2FbIIJh8fHhKcfXNXOLovdVBE7Z
-y682X1+R1lRK8D+vmQIDAQAB
------END PUBLIC KEY-----
-`)
+var xrsa *XRsa
 
-// 加密
-func RsaEncrypt(origData []byte) ([]byte, error) {
-	//解密pem格式的公钥
-	block, _ := pem.Decode(publicKey)
-	if block == nil {
-		return nil, errors.New("public key error")
+// 获取密钥
+func getRsaKey(fileName string) []byte {
+	b, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Print(err)
 	}
-	// 解析公钥
+	return b
+}
+
+func GetXrsa() *XRsa {
+	return xrsa
+}
+
+func InitXRsa(publicKeyPath, privateKeyPath string) error {
+	block, _ := pem.Decode(getRsaKey(publicKeyPath))
+	if block == nil {
+		return errors.New("public key error")
+	}
 	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	// 类型断言
 	pub := pubInterface.(*rsa.PublicKey)
-	//加密
-	return rsa.EncryptPKCS1v15(rand.Reader, pub, origData)
-}
 
-// 解密
-func RsaDecrypt(ciphertext []byte) ([]byte, error) {
-	//解密
-	block, _ := pem.Decode(privateKey)
+	block, _ = pem.Decode(getRsaKey(privateKeyPath))
 	if block == nil {
-		return nil, errors.New("private key error!")
+		return errors.New("private key error!")
 	}
-	//解析PKCS1格式的私钥
-	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	priv, err := x509.ParsePKCS8PrivateKey(block.Bytes)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	// 解密
-	return rsa.DecryptPKCS1v15(rand.Reader, priv, ciphertext)
+	pri, ok := priv.(*rsa.PrivateKey)
+	if ok {
+		xrsa = &XRsa{
+			publicKey:  pub,
+			privateKey: pri,
+		}
+	} else {
+		return err
+	}
+	return nil
 }
 
-// func main() {
-// 	data, _ := RsaEncrypt([]byte("hello world"))
-// 	fmt.Println(base64.StdEncoding.EncodeToString(data))
-// 	origData, _ := RsaDecrypt(data)
-// 	fmt.Println(string(origData))
-// }
+// 公钥加密
+func (r *XRsa) PublicEncrypt(data string) (string, error) {
+	partLen := r.publicKey.N.BitLen()/8 - 11
+	chunks := split([]byte(data), partLen)
+	buffer := bytes.NewBufferString("")
+	for _, chunk := range chunks {
+		bytes, err := rsa.EncryptPKCS1v15(rand.Reader, r.publicKey, chunk)
+		if err != nil {
+			return "", err
+		}
+		buffer.Write(bytes)
+	}
+	return base64.RawURLEncoding.EncodeToString(buffer.Bytes()), nil
+}
+
+// 私钥解密
+func (r *XRsa) PrivateDecrypt(encrypted string) (string, error) {
+	partLen := r.publicKey.N.BitLen() / 8
+	raw, err := base64.RawURLEncoding.DecodeString(encrypted)
+	//mylog.LogErr.Println("PrivateDecrypt base64 decode:",string(raw))
+	chunks := split([]byte(raw), partLen)
+	buffer := bytes.NewBufferString("")
+	for _, chunk := range chunks {
+		decrypted, err := rsa.DecryptPKCS1v15(rand.Reader, r.privateKey, chunk)
+		if err != nil {
+			return "", err
+		}
+		buffer.Write(decrypted)
+	}
+	return buffer.String(), err
+}
+
+func split(buf []byte, lim int) [][]byte {
+	var chunk []byte
+	chunks := make([][]byte, 0, len(buf)/lim+1)
+	for len(buf) >= lim {
+		chunk, buf = buf[:lim], buf[lim:]
+		chunks = append(chunks, chunk)
+	}
+	if len(buf) > 0 {
+		chunks = append(chunks, buf[:len(buf)])
+	}
+	return chunks
+}
